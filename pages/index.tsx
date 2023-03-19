c2d640401ac4fbb266c29b640f8cc485d889f4e0
Imbassy <yasser@imbassy.com>
2023-03-19 04:49:48 +0200

    stvmped

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,22 +11,22 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>RoomGPT</title>
+        <title>Stvmp</title>
       </Head>
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
         <a
-          href="https://vercel.fyi/roomGPT"
+          href="https://stvmp.com"
           target="_blank"
           rel="noreferrer"
           className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out hover:text-gray-300"
         >
           Clone and deploy your own with{" "}
-          <span className="text-blue-600">Vercel</span>
+          <span className="text-blue-600">Stvmp</span>
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
-          Generating dream rooms{" "}
+          Generating dream destinations{" "}
           <span className="relative whitespace-nowrap text-blue-600">
             <SquigglyLines />
             <span className="relative">using AI</span>
@@ -34,20 +34,20 @@ const Home: NextPage = () => {
           for everyone.
         </h1>
         <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400  text-gray-500 leading-7">
-          Take a Stvmp of your room and see how your room looks in different
+          Generate a Stvmp to see how your room looks in different
           themes. 100% free – remodel your room today.
         </h2>
         <Link
           className="bg-blue-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-blue-500 transition"
           href="/dream"
         >
-          Share your experience with real rovers
+          Share your experience with design gvrvs
         </Link>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
             <div className="flex sm:space-x-8 sm:flex-row flex-col">
               <div>
-                <h3 className="mb-1 font-medium text-lg">Know your next destination</h3>
+                <h3 className="mb-1 font-medium text-lg">Before Stvmp</h3>
                 <Image
                   alt="Original photo of a room with roomGPT.io"
                   src="/1.jpg"
@@ -57,9 +57,9 @@ const Home: NextPage = () => {
                 />
               </div>
               <div className="sm:mt-0 mt-8">
-                <h3 className="mb-1 font-medium text-lg">Generated Room</h3>
+                <h3 className="mb-1 font-medium text-lg">Stvmped</h3>
                 <Image
-                  alt="Generated photo of a room with roomGPT.io"
+                  alt="Generated photo of a room with stvmp.com"
                   width={400}
                   height={400}
                   src="/1-new.jpg"
